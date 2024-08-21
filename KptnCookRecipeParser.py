c0a81c0e84bda5f14eb9dd4a58707748bb6e10d4
Felix Engelhardt <felix.engelhardt@googlemail.com>
2024-08-21 15:06:41 +0200

    Start to implement class to handle recipe collection and adding first Json file for testing purposes

--- KptnCookRecipeParser.py
+++ KptnCookRecipeParser.py
@@ -1,16 +1,37 @@
 import json
 
+class KptnCookCollectionObject:
+    rawJSONString = None
+    collectionJSONObject = None
+
+    def __init__(self,rawJSONString=None):
+       self.setRawJSONString(rawJSONString)
+    
+    def setRawJSONString(self,inputRecipeJSONString):
+       self.rawJSONString = inputRecipeJSONString
+
+    def convertRawStringToJSONObject(self):
+        self.collectionJSONObject = json.loads(self.rawJSONString)
+
+
 class InputFileHandler:
     filename = ""
-    raw_content = ""
+    rawContent = ""
 
-    def ___init___(self, filename = ""):
+    def __init__(self, filename = ""):
         self.filename = filename
         with open(self.filename,"r") as inputFile:
-            self.raw_content = self.__readCompleteFileContent(inputFile)
+            self.rawContent = self.__readCompleteFileContent(inputFile)
 
     def __readCompleteFileContent(self, fileHandler):
         return fileHandler.readlines()
     
     def getCompleteFileContent(self):
-        return self.raw_content
+        return self.rawContent
+    
+
+if __name__ == "__main__":
+    inputFileHandler = InputFileHandler("kptncook_1.json")
+    kptnCookCollecction = KptnCookCollectionObject(inputFileHandler.getCompleteFileContent())
+    kptnCookCollecction.convertRawStringToJSONObject()
+    pass
